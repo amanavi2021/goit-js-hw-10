21db9af43c9e149272c730c1774a85a6d582564b
Nadiia Maltseva <amanavi2021@gmail.com>
2023-03-26 19:27:30 +0300

    updates index.js

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const DEBOUNCE_DELAY = 300;
 refs = {
     searchInput: document.querySelector('#search-box'),
     countriesLiss: document.querySelector('.country-list'),
-    countryInfo: document.querySelector('.country-info')
+    countryInfo: document.querySelector('.country-info'),
 
-}
+};
 
 // Notiflix.Notify.info('Cogito ergo sum');
