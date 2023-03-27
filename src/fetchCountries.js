0a8d8d5627486696772269fb72b7c27280acefb0
Nadiia Maltseva <amanavi2021@gmail.com>
2023-03-28 00:03:18 +0300

    Add styles.css

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -4,10 +4,10 @@ const BASE_URL = 'https://restcountries.com/v3.1';
 
 export function fetchCountries(name) {
     const searchParams = new URLSearchParams(
-        {}
+        {fields: 'name,capital,population,flags,languages', }
     );
-    const url =`${BASE_URL}/name/${name}`;
-
+    
+    const url =`${BASE_URL}/name/${name}?${searchParams}`;
     return fetch(url)
     .then(response=> {
         if (!response.ok) {
--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
 import { fetchCountries } from './fetchCountries';
 
-
 const DEBOUNCE_DELAY = 300;
 
 const refs = {
     searchInput: document.querySelector('#search-box'),
-    countriesLiss: document.querySelector('.country-list'),
+    countriesList: document.querySelector('.country-list'),
     countryInfo: document.querySelector('.country-info'),
 
 };
@@ -20,51 +19,74 @@ function onSearch(e) {
 
    const searchQuery = e.target.value.trim();
    if (searchQuery) {
-    // console.log(searchQuery);
-    fetchCountries(searchQuery)
+      fetchCountries(searchQuery)
     .then((result) => 
     {
         if (result.length >10) {
+            clearInfo();
             Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
         } 
-        if (2<= result.length <=10) {
+        if (2<= result.length && result.length <=10) {
+             refs.countryInfo.innerHTML ='';
+             renderCountries(result);
 
         }
         if (result.length === 1) {
-            renderCountry(result);
+             refs.countriesList.innerHTML ='';
+             renderCountry(result);
         }
-    
-        console.log(result.length);
-    }
+          
+    } 
     )
     .catch(error=> console.log(error))
 
+   } else {
+    clearInfo();
    }
    
 }
 
-function renderCountries(country) {
+function renderCountries(countries) {
+    
+    const markup = countries.map(({ flags, name })=> {
+          return `
+            <li class="country-list__item">
+                <img class="country-list__flag" src="${flags.svg}" alt="${name.official}" width="50" height="40">
+                <p class="country-list__name">${name.official}</p>
+            </li>
+      
+      `;
+    }).join('');
+     refs.countriesList.innerHTML = markup;
 
 }
 
 function renderCountry(country) {
-    console.log(country);
-    // const markup = countryCardTpl(country);
+    // console.log(country);
+    
     const markup = country.map(({ flags, name, capital, population, languages })=> {
+        const languagesList = parseLanguages(languages);
         return `
-      <div class="country-info__about">
-        <img class="country-info__flag" src="${flags.svg}" alt="${name.official}" width="50" height="40">
-        <p class="country-info__name">${name.official}</p>
-      </div>
-      <p class="country-info__text">Capital: <span class="country-info__capital-value">${capital}</span> </p>
-      <p class="country-info__text">Population: <span  class="country-info__population-value">${population}</span> </p>
-      <p class="country-info__text">Languages: <span  class="country-info__languages-value">${languages}</span> </p>
-      `;
+            <div class="country-info__about">
+                <img class="country-info__flag" src="${flags.svg}" alt="${name.official}" width="50" height="40">
+                <p class="country-info__name">${name.official}</p>
+            </div>
+            <p class="country-info__capital"><span class="country-info__text">Capital: </span>${capital}</p>
+            <p class="country-info__population"><span class="country-info__text">Population: </span>${population}</p>
+            <p class="country-info__languages"><span class="country-info__text">Languages: </span>${languagesList}</p>
+            `;
     })
      refs.countryInfo.innerHTML = markup;
 }
 
+function parseLanguages(languages) {
+    return Object.values(languages).join(', ')
+}
 
+function clearInfo() {
+    refs.countriesList.innerHTML ='';
+    refs.countryInfo.innerHTML =''; 
+}
 
 
 
